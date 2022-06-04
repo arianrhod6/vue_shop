9fe8c1d892da262d64094f7f5e25129c5cb74453
马洛 <1349853943@qq.com>
2022-06-04 10:37:27 +0800

    完成登录功能

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
 
+import './assets/fonts/iconfont.css'     //导入字体图标
+import './assets/css/global.css'         //导入全局样式
+
+import axios from 'axios'                //导入axios，写入vue组件，设置基础路径
+axios.defaults.baseURL = 'http://81.70.91.168:8888/api/private/v1/'
+Vue.prototype.$http = axios
+
 Vue.config.productionTip = false
 
 new Vue({
