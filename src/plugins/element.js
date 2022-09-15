1c73a87d54eda43199eb0a5af2f45b4a12aedd3d
马洛 <1349853943@qq.com>
2022-09-15 19:50:56 +0800

    订单功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -8,7 +8,8 @@ import { Dialog } from 'element-ui'
 import { MessageBox, Tag, Tree, Select, Option, Cascader } from 'element-ui'
 import { Alert, Tabs, TabPane, Steps, Step } from 'element-ui'
 import { CheckboxGroup, Checkbox, Upload } from 'element-ui'
-
+import Timeline from './timeline/index.js'
+import TimelineItem from './timeline-item/index.js'
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
@@ -45,6 +46,8 @@ Vue.use(Step)
 Vue.use(CheckboxGroup)
 Vue.use(Checkbox)
 Vue.use(Upload)
+Vue.use(Timeline)
+Vue.use(TimelineItem)
 
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
